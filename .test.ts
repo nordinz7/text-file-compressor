96c5bbcfc2aa9e16993b39afca02577ed913d59e
Nordin <vipnordin@gmail.com>
2024-08-24 23:45:49 +0800

    fix: Update file path in test case for frequency calculation

--- .test.ts
+++ .test.ts
@@ -1,15 +1,15 @@
-import { describe, test, expect ,beforeAll, afterAll} from 'bun:test'
+import { describe, test, expect } from 'bun:test'
 import { getFreq, readTextFile } from './utils'
 
-describe('frequency', async()=>{
-  const text = await readTextFile('/test.txt')
-  const freq = getFreq(text)
+describe('frequency', async () => {
+  const text = await readTextFile('/samples/_input.txt')
+  const freq = getFreq(await text.text())
   // There are 333 occurrences of ‘X’ and 223000 of ‘t’.
-  test('333 occurrences of X', ()=>{
+  test('333 occurrences of X', () => {
     expect(freq['X']).toBe(333)
   })
 
-  test('223000 occurrences of t', ()=>{
+  test('223000 occurrences of t', () => {
     expect(freq['t']).toBe(223000)
   })
 })
